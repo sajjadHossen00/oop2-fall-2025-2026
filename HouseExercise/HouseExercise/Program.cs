f8c1aa6958eb1223c411a01217e4a3e65b1c55e9
sajjadHossen00 <123761890+sajjadHossen00@users.noreply.github.com>
2025-12-03 12:49:26 +0600

    22-46799-1
    22-46799-1

---
+++ HouseExercise/HouseExercise/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+public class House
+{
+    public int YearBuilt { get; set; }
+    public double Size { get; set; } 
+    public House(int yearBuilt, double size)
+    {
+        YearBuilt = yearBuilt;
+        Size = size;
+    }
+    private int HowOld()
+    {
+        int currentYear = DateTime.Now.Year;
+        return currentYear - YearBuilt;
+    }
+    public bool CanBeSold()
+    {
+        return HowOld() > 15;
+    }
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"House built in {YearBuilt}, Size: {Size}");
+        Console.WriteLine($"Can be sold: {CanBeSold()}");
+    }
+}
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Enter the year the house was built: ");
+        int yearBuilt = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter the size of the house: ");
+        double size = Convert.ToDouble(Console.ReadLine());
+
+        House myHouse = new House(yearBuilt, size);
+        myHouse.DisplayInfo();
+    }
+}

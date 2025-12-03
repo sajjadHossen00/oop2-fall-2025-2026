f95990e2479fe340403abdf96a08cb3db1546322
sajjadHossen00 <123761890+sajjadHossen00@users.noreply.github.com>
2025-12-03 12:42:13 +0600

    22-46799-1
    22-46799-1

---
+++ Task3/Task3/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+double fahrenheit, celsius;
+Console.Write("Enter Celsius Degeree : ");
+celsius = Convert.ToDouble(Console.ReadLine());
+
+fahrenheit = (1.8 * celsius) + 32;
+Console.WriteLine($"Fahrenheit Degree : {fahrenheit}");

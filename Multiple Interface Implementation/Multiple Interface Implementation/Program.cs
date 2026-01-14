e12e1f2dc1497c06261ad40071dd7a2a4f3405e9
sajjadHossen00 <123761890+sajjadHossen00@users.noreply.github.com>
2026-01-14 22:23:34 +0600

    22-46799-1
    22-46799-1

---
+++ Multiple Interface Implementation/Multiple Interface Implementation/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+interface IPrintable
+{
+    void Print();
+}
+
+interface ISearizable
+{
+    void Save();
+}
+
+class Report : IPrintable, ISearizable
+{
+    public void Print()
+    {
+        Console.WriteLine("Printing the report");
+    }
+    public void Save()
+    {
+        Console.WriteLine("Saving the report");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Report r = new Report();
+        r.Print();
+        r.Save();
+    }
+}
